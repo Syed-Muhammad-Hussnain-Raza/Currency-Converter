d3da731a6186bc25b3287fb6bfcc07a3aa21cbc6
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-21 16:31:50 +0500

    Currency Converter App Files and Folder Uploaded Successfully

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-slate-300 h-screen">
+      <h1 className="bg-slate-500 text-white text-center p-4">
+        Currency Converter
+      </h1>
+    </div>
+  );
+}
+
+export default App;
