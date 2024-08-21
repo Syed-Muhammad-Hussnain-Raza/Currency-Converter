9ee13d2a7b3a14d3eec671b9e2ea926fb9c5f8e6
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-21 16:38:26 +0500

    App - Currency component imported and some styling done here using tailwand css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
+import Currency from "./components/Currency";
 
 function App() {
   return (
-    <div className="bg-slate-300 h-screen">
-      <h1 className="bg-slate-500 text-white text-center p-4">
-        Currency Converter
-      </h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-orange-200">
+      <h1 className="text-3xl font-bold">Currency Converter</h1>
+      <Currency />
     </div>
   );
 }
